1b559f1a77ba30cd2b258f7bb7d127c28358147b
Joel Young [Jooser PC] <joeleyoung01@gmail.com>
2025-02-28 13:58:11 -0600

    improved ui of search page

--- src/views/UserViews/ExploreRecipes.vue
+++ src/views/UserViews/ExploreRecipes.vue
@@ -21,32 +21,48 @@ async function getResults() {
     searched.value = true;
   }
 }
+
+watch(searchText, () => (searched.value = false));
 </script>
 
 <template>
   <v-container>
     <section>
-      <v-text-field
-        v-model="searchText"
-        label="Search recipes"
-        placeholder="Enter keywords to search..."
-        clearable
-        hide-details="auto"
-        variant="outlined"
-        rounded
-        @keyup.enter="getResults"
+      <div class="d-flex align-center gap-2">
+        <v-text-field
+          v-model="searchText"
+          label="Search recipes"
+          placeholder="Enter keywords to search..."
+          clearable
+          hide-details="auto"
+          variant="outlined"
+          rounded
+          @keyup.enter="getResults"
+        />
+        <v-btn icon color="primary" :loading="loading" @click="getResults">
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+      </div>
+
+      <v-alert
+        v-if="!recipes.length && !searchText && !loading && !searched"
+        type="info"
+        variant="tonal"
+        class="mt-2"
       >
-        <template #append-inner>
-          <v-btn icon variant="text" :disabled="loading" @click="getResults">
-            <v-icon>mdi-magnify</v-icon>
-          </v-btn>
-        </template>
-      </v-text-field>
+        Search Recipes by entering text in the search bar above!
+      </v-alert>
+      <v-alert
+        v-else-if="!recipes.length && searchText && !loading && searched"
+        type="info"
+        variant="tonal"
+        class="mt-2"
+      >
+        No recipes found matching your search.
+      </v-alert>
     </section>
 
     <section>
-      <v-progress-linear v-if="loading" indeterminate color="primary" class="mb-4" />
-
       <RecipeCard
         v-for="recipe in recipes"
         :key="recipe.id"
@@ -54,14 +70,6 @@ async function getResults() {
         class="mb-3"
         mode="public"
       />
-
-      <v-alert
-        v-if="!recipes.length && searchText && !loading && searched"
-        type="info"
-        variant="tonal"
-      >
-        No recipes found matching your search.
-      </v-alert>
     </section>
   </v-container>
 </template>
